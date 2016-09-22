04e5049d26a74256b00a85380dd4eb1f6c310d18
Matthew Tsui <matthew.zq.tsui@gmail.com>
2016-09-22 15:52:46 -0400

    Refactored Climbing Map for generic project dialog template

--- js/app.min.js
+++ js/app.min.js
@@ -8,64 +8,80 @@
 
     $scope.projects = [
       {
-        name: 'Neighbourhood Map',
-        desc: 'Climbing Gyms in SF Area',
+        name: 'Climbing Map',
+        titleBold: 'Climbing',
+        titleThin: 'Map',
+        skills: 'HTML5 . SCSS . Knockout . Google Maps API . Uber Rides API',
+        summary: 'Climbing Gyms in SF Area',
         img: 'img/neighbourhood_map.jpg',
         date: '2016',
-        dialog: 'projects/neighbourhood_map.tmpl.html'
+        dialog: 'projects/project_dialog.tmpl.html',
+        desc: 'A single-page web application using Knockout. The searchable webapp displays a Google Map of a few climbing gyms in the SF Bay Area. Users can click the listview or markers to see more information (including Uber pricing) on the selected gym.',
+        links: [
+          {
+            caption: 'Repo',
+            fa: 'fa-github',
+            href: 'https://github.com/mzqtsui/frontend-nanodegree-map'
+          },
+          {
+            caption: 'Demo',
+            fa: 'fa-globe',
+            href: 'https://mzqtsui.github.io/frontend-nanodegree-map/dist'
+          }
+        ]
       },
       {
         name: 'Time Bank',
-        desc: 'UX Design Project',
+        summary: 'UX Design Project',
         img: 'img/time.jpg',
         date: '2016',
         dialog: 'projects/timebank_dialog.tmpl.html'
       },
       {
         name: 'Bugs & Baubles',
-        desc: 'Frogger Clone',
+        summary: 'Frogger Clone',
         img: 'img/bug_game.jpg',
         date: '2016',
         dialog: 'projects/frogger_dialog.tmpl.html'
       },
       {
         name: 'Material Red',
-        desc: 'An experiment in SVG',
+        summary: 'An experiment in SVG',
         img: 'img/material_red.jpg',
         date: '2016',
         dialog: 'projects/material_red_dialog.tmpl.html'
       },
       {
         name: 'Upscale Markets',
-        desc: 'Grocery Shopping Prototype',
+        summary: 'Grocery Shopping Prototype',
         img: 'img/upscale.jpg',
         date: '2016',
         dialog: 'projects/upscale_markets_dialog.tmpl.html'
       },
       {
         name: 'Palette Website',
-        desc: 'Redesign, Development, Illustration',
+        summary: 'Redesign, Development, Illustration',
         img: 'img/palette.jpg',
         date: '2015',
         dialog: 'projects/palette_site_dialog.tmpl.html'
       },
       {
         name: 'PaletteApp2',
-        desc: 'Companion app for Palette hardware',
+        summary: 'Companion app for Palette hardware',
         img: 'img/paletteapp2.jpg',
         date: '2015',
         dialog: 'projects/paletteapp_dialog.tmpl.html'
       },
       {
         name: 'FastCards',
-        desc: 'Android flash card app',
+        summary: 'Android flash card app',
         img: 'img/fastcards_logo.jpg',
         date: '2015',
         dialog: 'projects/fastcards_dialog.tmpl.html'
       },
       {
         name: 'Slate',
-        desc: 'A 3D Projection-Mapped Unity Game',
+        summary: 'A 3D Projection-Mapped Unity Game',
         img: 'img/slate.jpg',
         date: '2014',
         dialog: 'projects/slate_dialog.tmpl.html'
@@ -79,6 +95,9 @@
         templateUrl: p.dialog,
         parent: angular.element(document.body),
         targetEvent: ev,
+        locals: {
+          project: p
+        },
         clickOutsideToClose:true,
         fullscreen: useFullScreen
       });
@@ -90,19 +109,16 @@
     }
 
   }]);
-  function DialogController($scope, $mdDialog) {
-    $scope.hide = function() {
-      $mdDialog.hide();
-    };
+
+
+  function DialogController($scope, $mdDialog, project) {
+    $scope.project = project;
     $scope.cancel = function() {
       $mdDialog.cancel();
     };
-    $scope.answer = function(answer) {
-      $mdDialog.hide(answer);
-    };
   }
 
-  DialogController.$inject = ['$scope', '$mdDialog'];
+  DialogController.$inject = ['$scope', '$mdDialog', 'project'];
 
   angular.element(document).ready(function() {
     angular.bootstrap(document, ['MainApp']);
